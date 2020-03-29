d37fca5b331c8d34dc5a5f92a6e5ffddbfc34dae
ar-ko <arTEAMko@icloud.com>
2020-03-29 21:30:47 +0500

    Add LoginScreen

---
+++ lib/RegisterScreen.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+
+class RegisterScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Ввод данных',
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+        ),
+        body: GestureDetector(
+          behavior: HitTestBehavior.opaque,
+          onTap: () {
+            FocusScope.of(context).requestFocus(new FocusNode());
+          },
+          child: Column(
+            children: [
+              Container(
+                  //color: Colors.blueGrey,
+                  margin: const EdgeInsets.fromLTRB(15, 23, 16, 0),
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          hintText: 'Имя',
+                          hintStyle: TextStyle(fontSize: 16),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 28,
+                      ),
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          hintText: 'Фамилия',
+                          hintStyle: TextStyle(fontSize: 16),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 28,
+                      ),
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          hintText: 'e-mail',
+                          hintStyle: TextStyle(fontSize: 16),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 28,
+                      ),
+                      TextFormField(
+                        keyboardType: TextInputType.number,
+                        decoration: const InputDecoration(
+                          hintText: 'Телефон',
+                          hintStyle: TextStyle(fontSize: 16),
+                        ),
+                      ),
+                    ],
+                  )),
+              Expanded(
+                child: Align(
+                  child: Container(
+                    child: RaisedButton(
+                      onPressed: () {},
+                      child: Text(
+                        'ПОЛУЧИТЬ КЛЮЧ',
+                        style: TextStyle(fontSize: 14, color: Colors.white),
+                      ),
+                      color: Theme.of(context).primaryColor,
+                    ),
+                    margin: const EdgeInsets.fromLTRB(0, 0, 0, 45),
+                  ),
+                  alignment: Alignment.bottomCenter,
+                ),
+              ),
+            ],
+          ),
+        ));
+  }
+}
