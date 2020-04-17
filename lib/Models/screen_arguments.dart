f4ff691048612c7dd26394b4203f5a9f61d5500c
ar-ko <arTEAMko@icloud.com>
2020-04-17 10:54:34 +0500

    Fix folder and file names

--- lib/Models/screen_arguments.dart
+++
@@ -1,9 +0,0 @@
-import '../models/user_data_for_registration.dart';
-
-
-class ScreenArguments {
-  final UserDataForRegistration user;
-  final String token;
-
-  ScreenArguments(this.user, this.token);
-}
